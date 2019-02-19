c7c3863547e3ac744325f09bb597d23f3ed02393
body33 <mkorzhyk@ukr.net>
2019-02-19 11:14:08 +0200

    third commit imya

---
+++ Client.java
@@ -0,0 +1,13 @@
+﻿
+public class Client {
+	
+	private String imya;
+	public Client(String i){
+		this.imya = i;
+	}
+	public void Jisty(Strava s){
+	   System.out.println("Клієнт" + "ім'я" + " з'їв страву(" +s.getClass().getName()+")");
+	}
+	
+
+}
