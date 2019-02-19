c651ac6e172a0418c5edfe593c15bb17d5825e00
body33 <mkorzhyk@ukr.net>
2019-02-19 08:36:20 +0200

    sposib copchena

---
+++ AbstractRestaurant.java
@@ -0,0 +1,13 @@
+﻿
+public class AbstractRestaurant {
+
+	public static void main(String[] args) {
+		
+		Client c = new Client("Котигорошко");
+		Strava s = new  NogaDynozavra(SposibPrygotuvannya.Copchena);
+		c.Jisty(s);
+		// TODO Auto-generated method stub
+
+	}
+
+}
