03e29e18b0c6358711e0256eed1f8c15cd6c4b9c
body33 <mkorzhyk@ukr.net>
2022-08-15 21:54:37 +0300

    добавлено ціну

---
+++ Strava.java
@@ -0,0 +1,35 @@
+
+public abstract  class Strava {
+String nazvaStravy;
+int price;
+public Strava(String nazvaStravy){
+	this.nazvaStravy=nazvaStravy;
+}
+	
+public Strava(){}		 
+	
+	 
+		 public  void Zgotuvaty(){
+		    	System.out.println("Страву зготовано");
+		    }	 
+}	 
+	
+
+
+	
+ 		
+	   
+
+	
+	
+		
+
+	
+	
+	
+
+	
+	
+
+   
+
