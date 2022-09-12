1b2d4f14ceac66f72edcf4c606b74918dce1b131
body33 <mkorzhyk@ukr.net>
2022-09-12 21:43:06 +0300

    додано конструктор SmazhenaTrava(String v)

---
+++ SmazhenaTrava.java
@@ -0,0 +1,14 @@
+
+public class SmazhenaTrava extends Strava {
+private String vydTravy;
+int price1 = 100;
+	public SmazhenaTrava(String v) {
+	this.vydTravy = v;
+	System.out.println("Салат " + vydTravy + " ціна:" + price1+ "грн.");
+}
+		
+	
+	
+	//салат,петрушка,кульбабки
+
+}
